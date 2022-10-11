0f5cbc4217bd729c912c43c1637e5531a086afb8
Abejo-Azriel <115531018+Abejo-Azriel@users.noreply.github.com>
2022-10-11 17:32:27 +0800

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int num1=4000
+    float num2=34.50;
+
+    printf("the total of value is %d",num1);
+    printf("the total value is is %f ,tempp);
+
+    return 0;
+
+}
